37ede3bcc3d9eb1179e3eac3a034bfc09155dcef
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-01-03 00:20:59 +0100

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
-import React from "react";
-
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="">
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+
+        <p>
+          <label>
+            Full Name: <input type="text" name="fullname" required />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email: <input type="email" name="email" required />
+          </label>
+        </p>
+        <p>
+          <label>
+            Phone Number: <input type="tel" name="phone" required />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" required></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send Message</button>
+        </p>
+      </form>
+    </div>
+  );
 }
